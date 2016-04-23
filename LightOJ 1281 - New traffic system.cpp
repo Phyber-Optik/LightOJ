2a555c8de335a18f9a2654b9c8d650133db39736
Towhidul Islam <im.towhidul@gmail.com>
2016-04-23 20:31:06 +0600

    Update LightOJ 1281 - New traffic system.cpp
    Read about the explanation:
    https://prisonbreakingbad.wordpress.com/2016/04/23/lightoj-1281-new-traffic-system/

--- LightOJ 1281 - New traffic system.cpp
+++ LightOJ 1281 - New traffic system.cpp
@@ -1,9 +1,7 @@
-/*  Towhidul Islam
-    University Of Dhaka
-    Problem Name :
-    Algorithm Used :
+/* 
+	Read about the solution in:
+	https://prisonbreakingbad.wordpress.com/2016/04/23/lightoj-1281-new-traffic-system/
 */
-
 #include<bits/stdc++.h>
 
 typedef long long ll;
